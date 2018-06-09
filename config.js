25346a859dedf0763df4c41b121b0e5256556200
Marco Maigua <marco.maigua1346@gmail.com>
2018-06-09 11:01:46 -0600

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+    consumer_key: 'UansMoWnioGiZMkeIPJQqguhh',  
+    consumer_secret: 'epQMAQETXGybzELDplrx844i6JcJgVUTm85xrWQKtfZxiBBLax',
+    access_token: '2540209423-mdbPT1f2H2BZ8hDXW9gStdk1iSDbiOZyPU482WO',  
+    access_token_secret: 'bJo4gQk7wt2WZMjB9LiZ9C8jX4cN1JFY2M6OhrGlByTlZ'
+  }
