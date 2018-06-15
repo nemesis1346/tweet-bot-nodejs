60305f41a5daf5510eb7b942481a1130bae8519f
nemesis1346 <marco.maigua1346@gmail.com>
2018-06-15 14:25:42 -0500

    orecofinguring new structure

--- postTweet.js
+++ postTweet.js
@@ -2,96 +2,166 @@ var twit = require('twit');
 var config = require('./config.js');
 var Twitter = new twit(config);
 // RETWEET BOT ==========================
+//TODO: MAKE BOT TO RESPOND MESSAGES AUTOMATICALLY
+//These functions are to retrieve the most popular twitts
+var hashtags=['#art',
+'#music',
+'#jazz',
+"#rock",
+'#spirit',
+'#chaman',
+'#santana',
+'#kichwa',
+'#ecuador',
+'#firstnations',
+'#indigenous',
+"#latin",
+"#diversidad",
+"#peace",
+"#elder",
+"#grandma",
+"#emprendedor",
+"#negocio",
+"#entrepreneur",
+"#startup",
+"#social",
+"#non-profit",
+"#blues",
+"#R&B",
+"#congress",
+"#concert",
+"#concerto",
+"#otavalo",
+"#mywaytrip",
+"#runa",
+"#hiphop"];
+//MUT COMBINE THESE TWITTS WITH THE ONES THAT I RETRIEVED
 
+function getHashTags(inputText) {  
+    var regex = /(?:^|\s)(?:#)([a-zA-Z\d]+)/gm;
+    var matches = [];
+    var match;
+
+    while ((match = regex.exec(inputText))) {
+        matches.push(match[1]);
+    }
+
+    return matches;
+}
+
+//-----------------------------------------------------------------------------------
+//This section is to post custom tags retrieved from the instructions above
 //ARRAY of MyWay Trip
 var hashtags = [
     {
-        "status": "This production was an attempts to show the most human side of our ancestors. #indigenous #latin #firstnations #ecuador #otavalo #diversidad #peace #elder #grandma. https://www.youtube.com/watch?v=5PpbDx-MieY",
-        "title": "PAYA MAMA VIDEO ingles"
+        "status": "https://www.youtube.com/watch?v=5PpbDx-MieY",
+        "title": "PAYA MAMA VIDEO ingles",
+        "description":"This production was an attempts to show the most human side of our ancestors."
     },
     {
-        "status": "Este trabajo es un esfuerzo por mostrar el lado más humano de nuestros taytas y mamas.  #indigenous #latin #firstnations #ecuador #otavalo #diversidad #peace #elder #grandma. https://www.youtube.com/watch?v=5PpbDx-MieY",
-        "title": "PAYA MAMA VIDEO espanol"
+        "status": "https://www.youtube.com/watch?v=5PpbDx-MieY",
+        "title": "PAYA MAMA VIDEO espanol",
+        "description":"Este trabajo es un esfuerzo por mostrar el lado más humano de nuestros taytas y mamas."
     },
     {
-        "status": "This is a small documentary about MyWay Trip music groups and several other artists and enterpreneurs. #entrepreneur #emprendedor #ecuador #otavalo #negocio. https://www.youtube.com/watch?v=KBJ3C50Ew-g",
-        "title": "emprendimiento ingles"
+        "status": "https://www.youtube.com/watch?v=KBJ3C50Ew-g",
+        "title": "emprendimiento ingles",
+        "description":"This is a small documentary about MyWay Trip music groups and several other artists and enterpreneurs."
     },
     {
-        "status": "Pequeño documental sobre varios emprendimientos de #Otavaleños en varias instancias sociales, inluyendo la banda MyWay Trip. #entrepreneur #startup #social #non-profit. https://www.youtube.com/watch?v=KBJ3C50Ew-g",
-        "title": "emprendimiento espanol"
+        "status": "https://www.youtube.com/watch?v=KBJ3C50Ew-g",
+        "title": "emprendimiento espanol",
+        "description":"Pequeño documental sobre varios emprendimientos de #Otavaleños en varias instancias sociales, inluyendo la banda MyWay Trip."
     },
     {
-        "status": " The artistic team was #AmadoMaigua and #AlejandraLema. We are very thankful in working with great musicians. #kichwa #jazz #rock #blues #R&B. https://www.youtube.com/watch?v=KuxiW9H6ueo",
-        "title": "yana yurak ingles"
+        "status": "https://www.youtube.com/watch?v=KuxiW9H6ueo",
+        "title": "yana yurak ingles",
+        "description":"The artistic team was #AmadoMaigua and #AlejandraLema. We are very thankful in working with great musicians."
     },
     {
-        "status": "Primer Sencillo y video clip del grupo musical #MyWayTrip. Este concepto fue nuestro primero intento de mostrar algo diferente. #kichwa #jazz #rock #blues #R&B. https://www.youtube.com/watch?v=KuxiW9H6ueo",
-        "title": "yana yurak espanol"
+        "status": "https://www.youtube.com/watch?v=KuxiW9H6ueo",
+        "title": "yana yurak espanol",
+        "description":"Primer Sencillo y video clip del grupo musical #MyWayTrip. Este concepto fue nuestro primero intento de mostrar algo diferente."
     },
     {
-        "status": "Thanks to the #PlataformSonica program for trusting in our work and giving us the permission to make use of the #NationalCongress of Ecuador to represent our first album #YanaYurak #congress #ecuador #concert #concierto. https://www.youtube.com/watch?v=xSru2GptCLs",
-        "title": "concierto asamblea general ingles"
+        "status": "https://www.youtube.com/watch?v=xSru2GptCLs",
+        "title": "concierto asamblea general ingles",
+        "description":"Thanks to the #PlataformSonica program for trusting in our work and giving us the permission to make use of the #NationalCongress of Ecuador to represent our first album #YanaYurak."
     },
     {
-        "status": "Gracias al programa Plataforma Sónica por confiar en nuestro trabajo y dejarnos hacer uso del espacio de la Asamblea Nacional del Ecuador para presentar nuestro primer album #YanaYurak #congress #ecuador #concert #concierto.. https://www.youtube.com/watch?v=xSru2GptCLs",
-        "title": "concierto asamblea general espanol"
-    },//TODO:KEEP DOING THIS 
-    {
-        "status": "In this project we try to evoque the sounds of the #andean #nostalgie from another perspective, from the #RUNAs, human beings that grow up in the #Andean #Region. https://www.youtube.com/watch?v=h5QaIYngEjU",
-        "title": "desesperanza ingles"
+        "status": "https://www.youtube.com/watch?v=xSru2GptCLs",
+        "title": "concierto asamblea general espanol",
+        "description":"Gracias al programa Plataforma Sónica por confiar en nuestro trabajo y dejarnos hacer uso del espacio de la Asamblea Nacional del Ecuador para presentar nuestro primer album #YanaYurak."
     },
     {
-        "status": "En este proyecto tratamos de evocar los sonidos de la regiòn andina. la nostalgia desde otra perspectiva, de los RUNAs, seres humanos que crecieron en la regiòn andina . https://www.youtube.com/watch?v=h5QaIYngEjU",
-        "title": "desesperanza general espanol"
+        "status": "https://www.youtube.com/watch?v=h5QaIYngEjU",
+        "title": "desesperanza ingles",
+        "description":"In this project we try to evoque the sounds of the #andean #nostalgie from another perspective, from the #RUNAs, human beings that grow up in the #Andean #Region."
     },
     {
-        "status": "Les presentamos #Sacha. Un tema que evoca los sonidos de la nostalgia de lo salvaje, de la nostalgia #kichwa, de historias y personajes que se han perdido en los tiempos en la tradición oral.  https://www.youtube.com/watch?v=v7ValD50B3A",
-        "title": "sacha espanol"
+        "status": "https://www.youtube.com/watch?v=h5QaIYngEjU",
+        "title": "desesperanza general espanol",
+        "description":"En este proyecto tratamos de evocar los sonidos de la regiòn andina. la nostalgia desde otra perspectiva, de los RUNAs, seres humanos que crecieron en la regiòn andina."
     },
     {
-        "status": "First live album Yana Yurak. With this production we are honoring the abstract art with an indigenous feeling. https://www.youtube.com/watch?v=iuoRuSuy0pY&t=5s",
-        "title": "yana yurak live espanol"
+        "status": "https://www.youtube.com/watch?v=v7ValD50B3A",
+        "title": "sacha espanol",
+        "description":"Les presentamos #Sacha. Un tema que evoca los sonidos de la nostalgia de lo salvaje, de la nostalgia #kichwa, de historias y personajes que se han perdido en los tiempos en la tradición oral."
     },
     {
-        "status": "Primer album en vivo Yana Yurak, el título de nuestro primer trabajo discográfico. Esta producción musical es un homenaje al arte abstracto con una nostalgia de la region andina. https://www.youtube.com/watch?v=iuoRuSuy0pY&t=5s",
-        "title": "yanda yurak espanol"
+        "status": "https://www.youtube.com/watch?v=iuoRuSuy0pY&t=5s",
+        "title": "yana yurak live espanol",
+        "description":"First live album Yana Yurak. With this production we are honoring the abstract art with an indigenous feeling."
     },
     {
-        "status": "MyWayTrip fest SBRNS en Vivo Machana Lounge-Otavalo. https://www.youtube.com/watch?v=i9w7Cb1fY-8",
-        "title": "concierto soberanos live espanol"
+        "status": "https://www.youtube.com/watch?v=iuoRuSuy0pY&t=5s",
+        "title": "yanda yurak espanol",
+        "description":"Primer album en vivo Yana Yurak, el título de nuestro primer trabajo discográfico. Esta producción musical es un homenaje al arte abstracto con una nostalgia de la region andina."
     },
     {
-        "status": "A Production of MyWay Trip featuring #SBRNS. A single with a social message. SBRNS is a #HipHop Band that criticize the status quo of the system by singing rap in native language #kichwa. https://www.youtube.com/watch?v=SYuglizZjf4",
-        "title": "yanda yurak english"
+        "status": "https://www.youtube.com/watch?v=i9w7Cb1fY-8",
+        "title": "concierto soberanos live espanol",
+        "description":"MyWayTrip fest SBRNS en Vivo Machana Lounge-Otavalo."
     },
     {
-        "status": "Una producción de MyWay Trip en conjunto con SBRNS. SBRNS es un grupo de Hip Hop que critica el status quo del sistema cantando rap en el idioma kichwa.  https://www.youtube.com/watch?v=SYuglizZjf4",
-        "title": "yanda yurak espanol"
+        "status": "https://www.youtube.com/watch?v=SYuglizZjf4",
+        "title": "yanda yurak english",
+        "description":"A Production of MyWay Trip featuring #SBRNS. A single with a social message. SBRNS is a #HipHop Band that criticize the status quo of the system by singing rap in native language #kichwa."
     },
     {
-        "status": "Transfiguration is our second single where we tried to project an intense work of composition and audiovisual production.  https://www.youtube.com/watch?v=5xOLLWQxjes",
-        "title": "transfiguracion english"
+        "status": "https://www.youtube.com/watch?v=SYuglizZjf4",
+        "title": "yanda yurak espanol",
+        "description":"Una producción de MyWay Trip en conjunto con SBRNS. SBRNS es un grupo de Hip Hop que critica el status quo del sistema cantando rap en el idioma kichwa."
     },
     {
-        "status": "Transfiguración es nuestro segundo sencillo en donde proyectamos un intenso trabajo de composición y producción audiovisual.  https://www.youtube.com/watch?v=5xOLLWQxjes",
-        "title": "transfiguracion espanol"
+        "status": "https://www.youtube.com/watch?v=5xOLLWQxjes",
+        "title": "transfiguracion english",
+        "description":"Transfiguration is our second single where we tried to project an intense work of composition and audiovisual production."
     },
     {
-        "status": "This is one of our first compositions. Was recorded in #Jatum #Rumi. According with some #Kichwa legends, this stone has several origins.  https://www.youtube.com/watch?v=zPIoG392Fws",
-        "title": "fandango jatun rumi english"
+        "status": "https://www.youtube.com/watch?v=5xOLLWQxjes",
+        "title": "transfiguracion espanol",
+        "description":"Transfiguración es nuestro segundo sencillo en donde proyectamos un intenso trabajo de composición y producción audiovisual. "
     },
     {
-        "status": "Este es uno de nuestras primeras composiciones. Se grabo en Jatum Rumi, segun leyendas kichwas esta piedra tienes muchos origenes https://www.youtube.com/watch?v=zPIoG392Fws",
-        "title": "fandango jatun rumi espanol"
+        "status": "https://www.youtube.com/watch?v=zPIoG392Fws",
+        "title": "fandango jatun rumi english",
+        "description":"This is one of our first compositions. Was recorded in #Jatum #Rumi. According with some #Kichwa legends, this stone has several origins."
     },
     {
-        "status": "Reggae al estilo Abdul https://www.youtube.com/watch?v=GM7aW-aeLyY",
-        "title": "abdul reggae  espanol"
+        "status": "https://www.youtube.com/watch?v=zPIoG392Fws",
+        "title": "fandango jatun rumi espanol",
+        "description":"Este es uno de nuestras primeras composiciones. Se grabo en Jatum Rumi, segun leyendas kichwas esta piedra tienes muchos origenes."
     },
     {
-        "status": "blues menor, nuestros inicios https://www.youtube.com/watch?v=F1GiTA8wvxc",
-        "title": "jam"
+        "status": "https://www.youtube.com/watch?v=GM7aW-aeLyY",
+        "title": "abdul reggae  espanol",
+        "description":"Reggae al estilo Abdul"
+    },
+    {
+        "status": "https://www.youtube.com/watch?v=F1GiTA8wvxc",
+        "title": "jam",
+        "description":"blues menor, nuestros inicios."
     },
     {
         "status": "https://open.spotify.com/album/6AfSoetp1twvTFMtcQnUC3",
@@ -166,6 +236,7 @@ var postTwitt = function () {
         }
     });
 }
+
 
 postTwitt();
 setInterval(postTwitt, 2100000);
